7c5895e4b75cc2cd2bf83eff814d713f3b515a10
YugBhanushali <yughbhanushali241103@gmail.com>
2023-12-28 16:15:19 +0530

    resolved cors error

--- server/index.js
+++ server/index.js
@@ -1,84 +1,91 @@
-const express = require("express")
-const http = require("http")
-const {Server} = require('socket.io')
-const dotenv = require('dotenv');
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+const dotenv = require("dotenv");
 dotenv.config();
 
-const app = express()
+const app = express();
 
-const httpServer = http.createServer(app)
+const allowedOrigins = ["http://localhost:3000", process.env.URL];
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
 
-app.get("/",(req,res)=>{
-  res.send("hello from server")
-})
+const httpServer = http.createServer(app);
 
-const allowedOrigins = ["http://localhost:3000",process.env.URL];
+app.get("/", (req, res) => {
+  res.send("hello from server");
+});
 
-const io = new Server(httpServer,{
+const io = new Server(httpServer, {
   cors: {
-    origin: allowedOrigins
-  }
-})
+    origin: allowedOrigins,
+  },
+});
 
 var records = new Map();
 const usersToUniquedID = new Map();
 const uniqueIdTousers = new Map();
 
-io.on("connection",(socket)=>{
+io.on("connection", (socket) => {
   // console.log(socket.id);
-  socket.on("joinRoom",(temp)=>{
-    socket.join(Number(temp))
-    records.set(socket.id,Number(temp))
-    socket.emit("ack",`You have joined room ${temp}`)
-  })
+  socket.on("joinRoom", (temp) => {
+    socket.join(Number(temp));
+    records.set(socket.id, Number(temp));
+    socket.emit("ack", `You have joined room ${temp}`);
+  });
 
-  socket.on("message",(temp)=>{
-    const roomNum = records.get(socket.id)
-    io.to(roomNum).emit("roomMsg",temp)
-  })
+  socket.on("message", (temp) => {
+    const roomNum = records.get(socket.id);
+    io.to(roomNum).emit("roomMsg", temp);
+  });
 
-  socket.on("details",(data)=>{
-    var user = data.socketId
-    var uniqueId = data.uniqueId
+  socket.on("details", (data) => {
+    var user = data.socketId;
+    var uniqueId = data.uniqueId;
 
-    usersToUniquedID.set(user,uniqueId)
-    uniqueIdTousers.set(uniqueId,user)
+    usersToUniquedID.set(user, uniqueId);
+    uniqueIdTousers.set(uniqueId, user);
     console.log("New User added");
     for (let [key, value] of usersToUniquedID) {
       console.log(key + " = " + value);
-      }
-  })
+    }
+  });
 
-  socket.on("send-signal",(temp)=>{
+  socket.on("send-signal", (temp) => {
     console.log(temp);
-    var to = temp.to
-    var socketOfPartner = uniqueIdTousers.get(to)
-    io.to(socketOfPartner).emit("signaling",{
-        from:temp.from,
-        signalData:temp.signalData,
-        to:temp.to
-    })
+    var to = temp.to;
+    var socketOfPartner = uniqueIdTousers.get(to);
+    io.to(socketOfPartner).emit("signaling", {
+      from: temp.from,
+      signalData: temp.signalData,
+      to: temp.to,
+    });
     // io.emit("receive-signal",temp)
-  })
+  });
 
-  socket.on("accept-signal",(temp)=>{
+  socket.on("accept-signal", (temp) => {
     console.log(temp);
-    var to = temp.to
-    var socketOfPartner = uniqueIdTousers.get(to)
+    var to = temp.to;
+    var socketOfPartner = uniqueIdTousers.get(to);
     console.log(socketOfPartner);
-    io.to(socketOfPartner).emit("callAccepted",{
-      signalData:temp.signalData,
-      to:temp.to
-  })
-  })
+    io.to(socketOfPartner).emit("callAccepted", {
+      signalData: temp.signalData,
+      to: temp.to,
+    });
+  });
 
-  socket.on('disconnect', () => {
+  socket.on("disconnect", () => {
     console.log(`Socket disconnected: ${socket.id}`);
-    
+
     // Remove the disconnected socket ID from maps
     const user = socket.id;
     const uniqueId = usersToUniquedID.get(user);
-    
+
     usersToUniquedID.delete(user);
     uniqueIdTousers.delete(uniqueId);
 
@@ -93,10 +100,8 @@ io.on("connection",(socket)=>{
       console.log(key + " = " + value);
     }
   });
-})
+});
 
-
-httpServer.listen(process.env.PORT || 8000,()=>{
+httpServer.listen(process.env.PORT || 8000, () => {
   console.log(`Listining on ${process.env.PORT ? process.env.PORT : "8000"}`);
-})
-
+});
