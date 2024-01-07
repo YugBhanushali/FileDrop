83ff17bb21db8bcc73d73a5ce89d0a644e056eba
YugBhanushali <yughbhanushali241103@gmail.com>
2024-01-07 18:35:25 +0530

    added vercel analytics

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import Navbar from "@/components/Navbar";
 import { keywordsForMetaData } from "@/utils/constants";
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -39,6 +40,7 @@ export default function RootLayout({
         >
           <Navbar />
           {children}
+          <Analytics/>
         </ThemeProvider>
       </body>
     </html>
