0dc7ca24b8148729a8fa0c39d9b06cb931b72e7e
YugBhanushali <yughbhanushali241103@gmail.com>
2024-10-12 23:18:19 +0530

    some changes in code generation

--- client/context/SocketProvider.tsx
+++ client/context/SocketProvider.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { nanoid } from "nanoid";
+import { customAlphabet, nanoid } from "nanoid";
 import React, {
   createContext,
   useContext,
@@ -13,22 +13,30 @@ import { Socket } from "socket.io-client/debug";
 const SocketContext = createContext<any>({});
 
 export const useSocket = () => {
-  const socket: { socket: Socket; userId: any,SocketId:any,setSocketId:any,peerState:any,setpeerState:any } = useContext(SocketContext);
+  const socket: {
+    socket: Socket;
+    userId: any;
+    SocketId: any;
+    setSocketId: any;
+    peerState: any;
+    setpeerState: any;
+  } = useContext(SocketContext);
   return socket;
 };
 
 export const SocketProvider = ({ children }: { children: React.ReactNode }) => {
   const socket = useMemo(() => {
-    
     return io(String(process.env.NEXT_PUBLIC_SOCKET_SERVER_URL));
   }, []);
-  const [peerState, setpeerState] = useState<any>()
-  const [SocketId, setSocketId] = useState<any>(socket)
-  const userId = useMemo(()=>{
-    return nanoid(10);
-  },[]);
+  const [peerState, setpeerState] = useState<any>();
+  const [SocketId, setSocketId] = useState<any>(socket);
+  const userId = useMemo(() => {
+    return customAlphabet("1234567890", 4);
+  }, []);
   return (
-    <SocketContext.Provider value={{ socket, userId,SocketId,setSocketId,peerState,setpeerState }}>
+    <SocketContext.Provider
+      value={{ socket, userId, SocketId, setSocketId, peerState, setpeerState }}
+    >
       {children}
     </SocketContext.Provider>
   );
